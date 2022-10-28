5c700aeea23282c216e29c3ddf88da84bceae6ba
abiolaakorede <abiolaakorede613@yahoo.com>
2022-10-28 02:13:59 +0100

    edited meal_contract

--- src/contracts/meal_contract.py
+++ src/contracts/meal_contract.py
@@ -6,15 +6,17 @@ class Meal:
         name = Bytes("NAME")
         image = Bytes("IMAGE")
         price = Bytes("PRICE")
+        description = Bytes("DESCRIPTION")
 
     def application_creation(self):
         return Seq([
-            Assert(Txn.application_args.length() == Int(3)),
+            Assert(Txn.application_args.length() == Int(4)),
             Assert(Txn.note() == Bytes("foodiz:uv1")),
             Assert(Btoi(Txn.application_args[2]) > Int(0)),
             App.globalPut(self.Variables.name, Txn.application_args[0]),
             App.globalPut(self.Variables.image, Txn.application_args[1]),
             App.globalPut(self.Variables.price, Btoi(Txn.application_args[2])),
+            App.globalPut(self.Variables.description, Txn.application_args[3]),
             Approve()
         ])
 
